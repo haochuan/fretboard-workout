8a4b0b758488d23f9f37efc1853cad75e24e4fa1
Haochuan Liu <haochuan.liu@gmail.com>
2016-10-22 12:13:56 -0700

    rewrite the scheduler from setiterval to settimeout

--- src/App.js
+++ src/App.js
@@ -4,33 +4,50 @@ import './App.css';
 import Chord from './components/Chord';
 import Controller from './components/Controller';
 
+// const INTERVAL = 1; // 1 sec
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {chord: 'C', start: false, lastChord: null};
+    this.state = {chord: 'C', scheduler: null, lastChord: null, duration: 3, currentTime: 0};
     this.getRandomInt = this.getRandomInt.bind(this);
     this.toggle = this.toggle.bind(this);
+    this.scheduler = this.scheduler.bind(this);
   }
 
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  toggle() {
-    if (this.state.start) {
-      clearInterval(this.state.start);
-      this.setState({start: null});
+  changeDuration(time) {
+    clearInterval(this.state.start);
+    this.setState({duration: time, scheduler: null});
+  }
+
+  scheduler() {
+    const {currentTime, duration} = this.state;
+    if (currentTime === duration - 1) {
+      const lastChord = this.state.lastChord;
+      const chordLibrary = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+      let randomNumber = this.getRandomInt(0, chordLibrary.length - 1);
+      // make sure not showing the same chord twice
+      while (lastChord === chordLibrary[randomNumber]) {
+        randomNumber = this.getRandomInt(0, chordLibrary.length - 1);
+      }
+      this.setState({chord: chordLibrary[randomNumber], lastChord: chordLibrary[randomNumber], currentTime: 0});
     } else {
-      this.setState({start: setInterval(() => {
-        const lastChord = this.state.lastChord;
-        const chordLibrary = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-        let randomNumber = this.getRandomInt(0, chordLibrary.length - 1);
-        // make sure not showing the same chord twice
-        while (lastChord === chordLibrary[randomNumber]) {
-          randomNumber = this.getRandomInt(0, chordLibrary.length - 1);
-        }
-        this.setState({chord: chordLibrary[randomNumber], lastChord: chordLibrary[randomNumber]});
-      }, 3000)});
+      this.setState({currentTime: currentTime + 1});
+    }
+
+    setTimeout(this.scheduler, 1000);
+  }
+
+  toggle() {
+    if (this.state.scheduler) {
+      clearTimeout(this.state.scheduler);
+      this.setState({chord: 'C', scheduler: null, lastChord: null, currentTime: 0});
+    } else {
+      this.setState({scheduler: setTimeout(this.scheduler, 0)});
     }
   }
 
